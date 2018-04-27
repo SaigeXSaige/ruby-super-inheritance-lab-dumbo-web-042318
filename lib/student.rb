01c1c68b45cccbb341a78f129f2445b9b167ebfc
Saige Leslie <saigeleslie@gmail.com>
2018-04-27 11:32:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,10 @@
 class Student
-    def raise_hand 
+    def hello
       puts "Hey there! I'm so excited to learn stuff."
     end
     
-    
+    def raise_hand
+      puts "Pick me!"
+    end
     
 end
